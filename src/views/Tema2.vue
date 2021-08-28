662b24e2ca0ca7f16e99cfe9ed4a261bfc6f9a45
WalterRoaSerrano <walter.roas@misena.edu.co>
2021-08-27 19:47:34 -0500

    Contenidos
    Pendiente estilos

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,30 +5,179 @@
     .titulo-principal
       .titulo-principal__numero
         span 2
-      h1 
+      h1 Modos y medios de transporte, conceptos de operación 
     .row.mb-5
       .col-lg-4
         img(src='@/assets/curso/t2/f1.png' alt='imagen relacionada')
       .col-lg-8
-        p 
+        p A medida que los sistemas de transporte y logística sigan integrándose, su impacto en el entorno físico (aire, agua y recursos terrestres) será más complejo. La globalización económica, la agilidad en la fabricación, la rapidez de la entrega y la administración en la cadena logística, están creando una mayor demanda de servicios de transporte intermodal y de infraestructuras de transporte multimodal. Para hacer frente a los impactos ambientales, el sector del transporte, sus clientes y partes interesadas deberán pasar de estrategias basadas en el cumplimiento de la normativa a otras que hagan hincapié en la gestión ambiental proactiva. 
         .cajon.color-primario.p-4.mb-4
-          p.mb-0 
+          p.mb-0 Los modos y medios de transporte manejan una serie de variables significativas al momento de fijar costos; por lo anterior, se hace necesario contar con una empresa operadora que atienda los requisitos de transporte según el producto, red de distribución acertada, además del cumpliendo normatividad vigente.
     #t_2_1.titulo-secundario
-      h2 
+      h2 2.1 Características de modos y medios de transporte
     .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5 
       .row.m-0.align-items-center.justify-content-between
-        .col-lg-4.mb-4.mb-lg-0
+        .col-lg-5.mb-4.mb-lg-0
           .titulo-sexto.color-acento-contenido
-            span 
+            span Características de los modos y medios de transporte
           img(src='@/assets/curso/t2/f2.svg' alt='imagen relacionada')
-        .col-lg-8
+        .col-lg-7
           .bloque-texto-a__texto.p-4
-            p 
-
-
-
-
-
+            p Los sistemas de cómo se moviliza la mercancía de un lugar a otro se denomina modo. Está dado por la combinación de redes, de vehículos y operaciones. Ahora el tipo de vehículo requerido para el transporte de carga se conoce como medio y es de acuerdo al modo seleccionado; puede ser camión, buque, avión y tren.
+    .cajon-b.p-4.mb-5
+      p.mb-0 Para seleccionar el modo de transporte adecuado, se debe tener en cuenta la capacidad de carga y los modos que se puedan utilizar, evaluando aspectos que describen la comparación y alternativas para estructurar los costos, entre ellos se encuentra la velocidad de circulación, disponibilidad, confiabilidad, capacidad y frecuencia. Algunos de estos elementos presentes para realizar el análisis de las diferentes alternativas que hay, son: 
+    .col-lg-4.mx-auto.mb-5
+      .titulo-sexto.color-acento-contenido
+        span Elementos según modos y medios 
+      figure
+        img(src='@/assets/curso/t2/f3.svg' alt='imagen relacionada')
+        figcaption Nota. Tomado de Bowersox, Closs y Cooper (2007)
+    p Finalmente, una vez se tengan identificados los medios y modos de transporte, se deben contemplar medidas que permitan un sistema de transporte intermodal, donde se realiza la identificación de centros industriales, puertos y fronteras; cadenas logísticas para transporte; localización de puntos estratégicos para intercambio modal de la mercancía o carga.
+    p.mb-5 Existen diferentes tipos de carga, entre los principales encontramos:
+    .col-lg-8.mx-auto.mb-5 
+      .titulo-sexto.color-acento-contenido
+        span Tipos de carga
+      img(src='@/assets/curso/t2/f3a.svg' alt='imagen relacionada')
+    .row.align-items-center.mb-5
+      .col-lg-5
+        .cajon.color-acento-contenido.p-4.mb-4
+          p.mb-0 Los diferentes tipos de carga se regularizan mediante el organismo denominado LegisComex, que es el sistema de inteligencia comercial para la gestión y análisis del comercio exterior y que permite tomar decisiones en los negocios internacionales. Usted podrá consultar en la biblioteca SENA, los tipos de carga, de acuerdo con este sistema de regulación.
+      .col-lg-7
+        .tarjeta.color-secundario.p-3.mb-5
+          .row.justify-content-around.align-items-center
+            .col-3.col-sm-2.col-lg-2
+              img(src="@/assets/curso/t1/f39.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  h4 Tipos de carga según Legiscomex
+                  p.mb-0.text-small Le invitamos a consultar los tipos de carga, que podrá encontrar en la biblioteca SENA, visitándola mediante el enlace:
+                .col-sm-auto
+                  a.boton.color-acento-contenido(:href="obtenerLink('https://login.bdigital.sena.edu.co/login?url=https://www.legiscomex.com/Documentos/TIPOSDECARGA')" target="_blank")
+                    span Enlace web
+                    i.fas.fa-link
+    Separador
+    #t_2_2.titulo-secundario
+      h2 2.2 Plan de ruta
+    .row.align-items-center.mb-5
+      .col-lg-7
+        .cajon-b.color-primario.p-4.mb-4
+          p.mb-0 Para diseñar, optimizar y planificar las rutas de transporte se debe conocer de primera mano el tipo de carga, vehículo, área y concurrencia de tráfico al destino del producto; intervienen varios elementos, el principal es la tecnología; un software que cubra todas las necesidades requeridas durante el proceso, reduce tiempos de trayecto, desgaste de los vehículos y mejora entregas a clientes, lo que se refleja en un servicio al cliente.
+        p Todo esto se puede alcanzar observando la información donde se encuentran los productos a despachar, clientes a servir y agradar, cantidades de carga transportada, entre otros.
+        p Al elaborar un plan de ruta, los administradores pueden verse en dificultades en el proceso de las variables que se deben tener en cuenta, impidiendo su efectividad, operatividad y comercialización. Esto lleva a un desafío aún mayor para los planificadores de los manuales de ruta, evitando la proyección real de estas variables. Algunos ejemplos de esto son:
+      .col-lg-5
+        img(src='@/assets/curso/t2/f4.jpg' alt='imagen relacionada')
+    .col-lg-11.mx-auto.mb-5
+      .titulo-sexto.color-acento-contenido
+        span Plan de ruta
+      img(src='@/assets/curso/t2/f5.svg' alt='imagen relacionada')
+    Separador
+    #t_2_3.titulo-secundario
+      h2 2.3 Documentación de transporte internacional
+    p.mb-5 Los encargados de coordinar y organizar los embarques, consolidar carga según acuerdos legales en el territorio son los agentes de carga. 
+    .col-lg-11.mx-auto.mb-5
+      .row.align-items-end
+        .col-lg-6
+          .titulo-sexto.color-acento-contenido
+            span Agentes de carga
+          img(src='@/assets/curso/t2/f6.png' alt='imagen relacionada')
+        .col-lg-6
+          .row.align-items-center
+            .col-lg-6
+              a(href='#/curso/tema2/#1')
+                .himg1.p-4
+                  h4.mb-0(style='margin-top:80px;position:relative') Agente de carga marítimo
+            .col-lg-6
+              a(href='#/curso/tema2/#2')
+                .himg1.p-4
+                  h4.mb-0(style='margin-top:80px;position:relative') Agente de carga aérea
+            .col-lg-6
+              a(href='#/curso/tema2/#3')
+                .himg1.p-4
+                  h4.mb-0(style='margin-top:80px;position:relative') Agente de carga terrestre
+            .col-lg-6
+              a(href='#/curso/tema2/#4')
+                .himg1.p-4
+                  h4.mb-0(style='margin-top:80px;position:relative') Agente de carga ferroviarios
+    p.mb-5 Existen diferentes tipos de agentes de carga, de acuerdo con los modos de transporte de la mercancía:
+    .row.justify-content-between.mb-5#1
+      .col-auto
+      .col-lg-1
+        a(:href="obtenerLink('#/curso/tema2/#t_2_3')")
+          img(src='@/assets/curso/up.svg' alt='Imagen relacionada' style='max-width:38px').mx-auto
+    .row.mb-5
+      .col-lg-5
+        img(src='@/assets/curso/t2/f7.png' alt='imagen relacionada').mb-4
+      .col-lg-7
+        h3 Agente de carga marítimo
+        p Es el encargado de transportar mercancía a través del mar y son los entes autorizados de manejo de las legislaciones nacionales para su intermediación ante las autoridades portuarias y marítimas en el manejo de la carga por este modo.
+    img(src='@/assets/curso/t2/f8alt.jpg' alt='imagen relacionada').mb-4
+    .row.justify-content-between.mb-5#2 
+      .col-auto
+      .col-lg-1
+        a(:href="obtenerLink('#/curso/tema2/#t_2_3')")
+          img(src='@/assets/curso/up.svg' alt='Imagen relacionada' style='max-width:38px').mx-auto
+    .row.mb-5
+      .col-lg-5
+        img(src='@/assets/curso/t2/f9.jpg' alt='imagen relacionada').mb-4
+      .col-lg-7
+        h3 Agente de carga aérea
+        p Encargados de brindar apoyo y asesoría en el transporte aéreo, deben ser certificados Agencia Internacional de Agentes de Carga Aérea (IATA) Ofrecen asesoría en rutas, documentación, capacidad de carga dentro del avión, entrega de producto y acompañamiento hasta destino final.
+        .cajon.color-acento-contenido.p-4
+          p.mb-0 Los agentes de carga marítima y aéreo, pueden cumplir la función de consolidar la carga de un mismo destino, para despachar las mercancías en bloque con una sola guía aérea o un solo conocimiento de embarque y con un solo transportador. Esta labor resulta de mucha utilidad, pues el agente logra una tarifa especial, ya que maneja pesos y volúmenes de mayor tamaño.
+    .row.justify-content-between.mb-5#3 
+      .col-auto
+      .col-lg-1
+        a(:href="obtenerLink('#/curso/tema2/#t_2_3')")
+          img(src='@/assets/curso/up.svg' alt='Imagen relacionada' style='max-width:38px').mx-auto
+    .row.mb-5
+      .col-lg-5
+        img(src='@/assets/curso/t2/f10.jpg' alt='imagen relacionada').mb-4
+      .col-lg-7
+        h3 Agente de carga terrestre
+        p Este agente debe asegurar que el envío de la carga se ajuste a todas las normas a nivel nacional; para el caso de Colombia, es el Ministerio de Transporte, además le presta ayuda al cliente para el traslado de la mercancía por vía terrestre y las transacciones fronterizas.
+    .row.justify-content-between.mb-5#4
+      .col-auto
+      .col-lg-1
+        a(:href="obtenerLink('#/curso/tema2/#t_2_3')")
+          img(src='@/assets/curso/up.svg' alt='Imagen relacionada' style='max-width:38px').mx-auto
+    .row.mb-5
+      .col-lg-5
+        img(src='@/assets/curso/t2/f11.jpg' alt='imagen relacionada').mb-4
+      .col-lg-7
+        h3 Agentes de carga ferroviarios
+        p Encargados del transporte de carga por tren, efectúa la elaboración del conocimiento de embarque, manejo de la carga, documentación, uso de espacio, utilización de carga y suministro de tarifas, entre otros.
+        .cajon.color-acento-contenido.p-4
+          p.mb-0 En la actualidad existen empresas que tienen su departamento de transporte logístico y hacen las actividades propias del agente aduanero; aunque normalmente son las grandes empresas quienes implementan estos departamentos. 
+    p Para aquellas pequeñas y medianas empresas, que deben que contratar dichos servicios, es importante que tengan en cuenta: 
+    ul.lista-ul
+      li 
+        i.fas.fa-check.cs.ms-4
+        | Servicios del agente de carga que no incrementen los costos de transportación. 
+      li 
+        i.fas.fa-check.cs.ms-4
+        | La capacidad de buscar opciones de transportación y obtener las mejores tarifas. 
+      li 
+        i.fas.fa-check.cs.ms-4
+        | Que tenga experiencia para brindar mejores soluciones cuando se presenten problemas. 
+      li 
+        i.fas.fa-check.cs.ms-4
+        | Que se mantengan actualizados en legislación aduanera, tributación, certificaciones y medios de transporte, rutas, tarifas y líneas. 
+      li 
+        i.fas.fa-check.cs.ms-4
+        | Que se practiquen los valores de honestidad, ética, justicia, entre otros. 
+    .row
+      .col-lg-5
+        img(src='@/assets/curso/t2/f12.jpg' alt='imagen relacionada').mb-4
+      .col-lg-7
+        .cajon-b.color-primario.p-4
+          p Los agentes de carga en cualquier modalidad, ofrecen soluciones integrales para resolver problemas logísticos y de transporte.
+        p La principal función de los agentes de carga es brindar soluciones integrales de logística y de transporte a las empresas, de forma que solo se preocupan por cumplir lo que mejor saben hacer, sus productos y/o servicios.
+        a.anexo.mb-4(href="https://www.asonav.org/documentos/" target="_blank")
+          .anexo__icono
+            img(src="@/assets/template/icono-link.svg")
+          .anexo__texto
+            p Le invitamos a consultar la página de Asonav, en donde podrá tener información detallada respecto a autoridad marítima, legislación aduanera y sector portuario; términos que hacen parte del comercio exterior vigente:
 
 
 
