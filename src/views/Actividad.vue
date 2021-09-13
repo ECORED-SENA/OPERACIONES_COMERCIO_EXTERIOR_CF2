b2b522b4b82171e5c43a5253f7558eeb79333b07
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-09-13 09:54:36 -0500

    Ajuste actividad

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -10,7 +10,7 @@
             img(src="@/assets/template/emparejamiento.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Actividad didáctica 
+          h2 Qué sabemos de transporte de mercancía
         p.mb-4 De acuerdo con los conceptos emitidos acerca a los medios y modos de transporte, relacione las letras en las casillas que corresponda a la definición citada y luego haga clic en el botón Enviar para validar su resultado.
         .tarjeta.tarjeta--azul.p-3
           .row.justify-content-around.align-items-center            
